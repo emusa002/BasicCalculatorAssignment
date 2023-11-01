32e28cdb0c89afa12052afa2fcfe1191bbeaf762
Miika Reijonen <hmire02@xamk.fi>
2023-11-01 10:46:33 +0200

    Add project files.

---
+++ BasicCalculatorAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BasicCalculatorAssignment
+{
+    internal class Program
+    {
+        // The goal of this assignment is to learn how to create and use methods in our code
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Simple Calculator");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Subtract");
+                Console.WriteLine("3. Multiply");
+                Console.WriteLine("4. Divide");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Choose an operation: ");
+
+                // Task 1. Read choice input and assing it to new int variable
+
+                // Task 2. Check if user selected choice 5. If so then exit the loop
+
+                Console.Write("Enter first number: ");
+                // Task 3. Get the first number and assing it to new double variable
+
+                Console.Write("Enter second number: ");
+                // Task 4. Get the second number and assing it to new double variable
+
+                // Task 5. Create double variable for default result value and assign it to be 0
+
+                // Task 6. Handle choices using switch statement
+                // Task 6.1 For each case create new function/method an call it inside the case
+                //          - Case 1: Add 
+                //          - Case 2: Subtract
+                //          - Case 3: Multiply
+                //          - Case 4: Divide, Note remeber to handle 0
+                // Note: Think what the default choice should be
+                // Note: Declare each case function/method outside Main method
+                //       There is comment below where you can place your methods
+
+                // Task 7. Print out the result
+            }
+        }
+
+        // Declare your methods/functions here
+    }
+}
